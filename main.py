803a9e7a75959bf0c74eb0e680610a6605374a30
Cristian Gonzalez <alexsiscg@gmail.com>
2025-07-07 08:17:48 -0600

    solution to problem from samsung

---
+++ main.py
@@ -0,0 +1,31 @@
+def sum_pair(array):
+    array = list(map(str, array))
+    h = {}
+    for i in array:
+        if len(i) > 2:
+            s = i[0] + i[-1]
+            if s[0] == s[-1] and s not in h:
+                h[s] = [int(i)]
+            else:
+                h[s].append(int(i))
+    # print(h)
+    maxn = 0
+    for key in h:
+        if len(h[key]) >= 2:
+            h[key].sort()
+            print(h)
+            maxn = max(maxn, h[key][-1] + h[key][-2])
+    return maxn
+
+
+def main():
+    testcase = [101, 1991]
+    testcase1 = [101, 2]
+    testcase2 = [101, 2002, 1991, 2442]
+    testcase3 = [1, 2, 3, 4, 101, 838, 828, 808]
+    testcase4 = [1]
+    return sum_pair(testcase3) if sum_pair(testcase3) > 0 else -1
+
+
+if __name__ == "__main__":
+    print(main())
